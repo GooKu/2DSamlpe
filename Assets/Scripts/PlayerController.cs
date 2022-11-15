ed042ff29c4f4d8889df87dc92790c716b359944
goo <test@g.com>
2022-11-15 15:49:46 +0800

    remove 111

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
-//testa111
+//testa
 public class PlayerController : MonoBehaviour
 {
     public event Action ArrivedGoalEvent;
